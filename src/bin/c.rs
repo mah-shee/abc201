41ef49e06f8df8b08192e4278fdf62bd769977e8
mah-shee <masa@masatoshi.local>
2021-08-27 20:19:13 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,28 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s:Chars,
     }
+    let mut ans = 0;
+    for i in 0..=9999 {
+        let mut x = i;
+        let mut flag = vec![false; 10];
+        for _ in 0..4 {
+            flag[x % 10] = true;
+            x /= 10;
+        }
+        let mut flag2 = true;
+        for j in 0..10 {
+            if s[j] == 'o' && !flag[j] {
+                flag2 = false;
+            }
+            if s[j] == 'x' && flag[j] {
+                flag2 = false;
+            }
+        }
+        if flag2 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
